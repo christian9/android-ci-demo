a6f9f52efb96b6732cd512cc8fe60651c2255b98
Paolo Angelo <39821263+Paolo-Man001@users.noreply.github.com>
2020-05-13 22:30:05 +1200

    add all and commit

---
+++ app/src/main/java/com/paolomanlunas/androidcidemoapp/MainActivity.java
@@ -0,0 +1,28 @@
+package com.paolomanlunas.androidcidemoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+
+
+   }
+
+   public void goToNextPage(View view) {
+//      Toast.makeText(this,"You clicked a button!", Toast.LENGTH_SHORT).show();
+      Intent intent = new Intent(this, SecondActivity.class);
+      startActivity(intent);
+   }
+}
